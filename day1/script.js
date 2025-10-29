4516b4a7eaf7e70e1347d569beebeb73efa46ebd
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-10-29 23:26:51 +0530

    my first day of problem solving

---
+++ day1/script.js
@@ -0,0 +1,23 @@
+// Question List
+
+// 1. Print Numbers from 1 to N
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+// 2. Print Numbers from N to 1 without changing the loop condition of above question
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+// 3. Print All Even Numbers from 1 to N
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// 4.
