4a82764f56acefe675f27f7cde1d2f0c6c081c24
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2026-01-15 09:11:57 +0530

    day 8

--- Month-01/day-8.js
+++ Month-01/day-8.js
@@ -1,1 +1,36 @@
 //- **Day 8:** Create a simple calculator (+, −, ×, ÷)
+
+function calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+
+    case "*":
+      result = num1 * num2;
+      break;
+
+    case "-":
+      result = num1 - num2;
+      break;
+
+    case "/":
+      if (num2 === 0) {
+        return "Division by zero is not allowed";
+      }
+      result = num1 / num2;
+      break;
+
+    default:
+      return "Invalid operator";
+  }
+  return result;
+}
+
+console.log(calculator(10, 5, "+")); // 15
+console.log(calculator(10, 5, "-")); // 5
+console.log(calculator(10, 5, "*")); // 50
+console.log(calculator(10, 5, "/")); // 2
+console.log(calculator(10, 0, "/")); // Error message
