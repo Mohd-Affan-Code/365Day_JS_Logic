f33bea9c5ac2e8f14a8307b8666a5e15f9555022
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2026-01-27 23:22:15 +0530

    day 19

---
+++ Month-01/day-19.js
@@ -0,0 +1,16 @@
+// - **Day 19:** Print all prime numbers between 1 to 100
+
+for (let num = 2; num <= 100; num++) {
+  let isPrime = true;
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(num);
+  }
+}
