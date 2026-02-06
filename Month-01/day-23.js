aac99a4d2eee9e7c50d3bd89789c015efba19390
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2026-02-06 09:48:37 +0530

    day 22

---
+++ Month-01/day-23.js
@@ -0,0 +1,14 @@
+// - **Day 22:** Find sum and average of array elements
+
+let arr = [10, 20, 30, 40, 50];
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+let average = sum / arr.length;
+
+console.log("Sum:", sum);
+console.log("Average:", average);
