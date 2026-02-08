d10fda0217be2cb3a6ff0bb6fea5fb7c1ab160b3
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2026-02-08 07:46:43 +0530

    day 25 find the second largest number

--- Month-01/day-25.js
+++ Month-01/day-25.js
@@ -1,1 +1,20 @@
 //- **Day 25:** Find the second largest element in an array
+let arr = [10, 5, 20, 8, 20];
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > largest) {
+    secondLargest = largest;
+    largest = arr[i];
+  } else if (arr[i] > secondLargest && arr[i] !== largest) {
+    secondLargest = arr[i];
+  }
+}
+
+if (secondLargest === -Infinity) {
+  console.log("Second largest element does not exist");
+} else {
+  console.log("Second largest element:", secondLargest);
+}
